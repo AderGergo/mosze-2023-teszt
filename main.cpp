4f045efebee058af0a8c2aca22a8123e5842469f
AderGergo <gergo.ader@gmail.com>
2026-02-23 12:50:44 +0100

    mukodik

--- main.cpp
+++ main.cpp
@@ -4,42 +4,37 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    // NELEMENTS nincs definiálva helyesen: N_ELEMENTS
-    int *b = new int[NELEMENTS];
+   
+    int *b = new int[N_ELEMENTS]; 
+    
+   
+    std::cout << "1-100 ertekek duplazasa" << std::endl; 
 
-    // Hiányzó pontosvessző a sor végéről
-    // A ' ' karakterekhez való (1 betű), szöveghez " " kell
-    std::cout << '1-100 ertekek duplazasa'
-
-    // A for ciklusból hiányzik a feltétel és a növekmény 
-    for (int i = 0;)
+    
+    for (int i = 0; i < N_ELEMENTS; i++) 
     {
-        b[i] = i * 2;
+        b[i] = (i + 1) * 2; 
     }
 
-    // A feltétel csak egy 'i', ami 0-nál azonnal leáll, így a ciklus le sem fut
-    for (int i = 0; i; i++)
+    
+    for (int i = 0; i < N_ELEMENTS; i++) 
     {
-        //  Hiányzó pontosvessző a sor végéről
-        std::cout << "Ertek:"
+        std::cout << "Ertek: " << b[i] << std::endl; 
     }    
 
     std::cout << "Atlag szamitasa: " << std::endl;
 
-    // Nincs kezdőérték (0), így az atlag változó memóriaszeméttel fog számolni
-    int atlag;
+    double atlag = 0; 
 
-    // A for ciklusban pontosvessző ; kellene a vessző helyett
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        // Hiányzó pontosvessző a sor végéről
-        atlag += b[i]
+        atlag += b[i]; 
     }
 
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
 
-    // Memóriaszivárgás!
+    delete[] b;
 
     return 0;
 }
