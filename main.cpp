653a9f66cf5572eaf5524ca6e05db4b9a1a636be
AderGergo <gergo.ader@gmail.com>
2026-02-23 12:45:18 +0100

    komment

--- main.cpp
+++ main.cpp
@@ -4,23 +4,42 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
+    // NELEMENTS nincs definiálva helyesen: N_ELEMENTS
     int *b = new int[NELEMENTS];
+
+    // Hiányzó pontosvessző a sor végéről
+    // A ' ' karakterekhez való (1 betű), szöveghez " " kell
     std::cout << '1-100 ertekek duplazasa'
+
+    // A for ciklusból hiányzik a feltétel és a növekmény 
     for (int i = 0;)
     {
         b[i] = i * 2;
     }
+
+    // A feltétel csak egy 'i', ami 0-nál azonnal leáll, így a ciklus le sem fut
     for (int i = 0; i; i++)
     {
+        //  Hiányzó pontosvessző a sor végéről
         std::cout << "Ertek:"
     }    
+
     std::cout << "Atlag szamitasa: " << std::endl;
+
+    // Nincs kezdőérték (0), így az atlag változó memóriaszeméttel fog számolni
     int atlag;
+
+    // A for ciklusban pontosvessző ; kellene a vessző helyett
     for (int i = 0; i < N_ELEMENTS, i++)
     {
+        // Hiányzó pontosvessző a sor végéről
         atlag += b[i]
     }
+
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+
+    // Memóriaszivárgás!
+
     return 0;
 }
